ebaf81068fe2ca3c058d32599fc4969987cd100e
nnfedorov <nikola_fed1@mail.ru>
2020-09-08 11:28:53 +0300

    refs #18478 touch scroller

--- projects/swimlane/ngx-datatable/src/lib/components/body/scroller.component.ts
+++ projects/swimlane/ngx-datatable/src/lib/components/body/scroller.component.ts
@@ -42,7 +42,6 @@ export class ScrollerComponent implements OnInit, OnDestroy {
   prevScrollXPos: number = 0;
   element: any;
   parentElement: any;
-  onScrollListener: any;
 
   private _scrollEventListener: any = null;
 
@@ -57,6 +56,8 @@ export class ScrollerComponent implements OnInit, OnDestroy {
       this.parentElement = renderer.parentNode(renderer.parentNode(this.element));
       this._scrollEventListener = this.onScrolled.bind(this);
       this.parentElement.addEventListener('scroll', this._scrollEventListener);
+
+      this.parentElement.addEventListener('mousedown', this.onMouseDownListener); // #18478
     }
   }
 
@@ -64,6 +65,11 @@ export class ScrollerComponent implements OnInit, OnDestroy {
     if (this._scrollEventListener) {
       this.parentElement.removeEventListener('scroll', this._scrollEventListener);
       this._scrollEventListener = null;
+    }
+
+    if (this.onMouseDownListener) {
+      // #18478
+      this.parentElement.removeEventListener('mousedown', this.onMouseDownListener);
     }
   }
 
@@ -98,5 +104,107 @@ export class ScrollerComponent implements OnInit, OnDestroy {
 
     this.prevScrollYPos = this.scrollYPos;
     this.prevScrollXPos = this.scrollXPos;
+  }
+
+  /***** Touch Scroll *****/
+  @Input() scrollH = true;
+  @Input() scrollV = true;
+  @Input() tipDelayInMs = 100;
+  @Input() moveThresholdInPx = 10; // defines the minimum offset that we'd like to consider as 'move'
+
+  touchScrollMove = false; // prevent interactions when view is being moved
+
+  private cursorTimer?: any;
+  private touchScrollOn = false;
+  private initX: number;
+  private initY: number;
+
+  onMouseDownListener = (event: MouseEvent) => {
+    // console.log('onMouseDown()', event);
+    if (event.button === 0) {
+      // left button pressed
+      this.initX = event.pageX;
+      this.initY = event.pageY;
+
+      this.parentElement.addEventListener('mousemove', this.onMouseMoveFn);
+
+      document.addEventListener('mouseup', this.onDocumentMouseUpFn);
+      document.addEventListener('dragend', this.onDocumentDragEndFn); // fixes missed document's 'mouseup' event
+
+      this.setCursor();
+    }
+  };
+
+  private onMouseMoveFn = (event: MouseEvent) => {
+    // console.log('onMouseMove()', event);
+    if (!this.touchScrollOn) {
+      this.parentElement.classList.add('touch-scroll_on'); // switches off scroll snap in advance to prevent jumping
+      this.touchScrollOn = true;
+    }
+
+    if (!this.touchScrollMove) {
+      // we should avoid preventing elements activation without moving view
+      if (
+        Math.abs(this.initX - event.pageX) > this.moveThresholdInPx ||
+        Math.abs(this.initY - event.pageY) > this.moveThresholdInPx
+      ) {
+        this.parentElement.classList.add('touch-scroll_move');
+        this.touchScrollMove = true;
+      } else {
+        // return; // no move
+      }
+    }
+
+    if (this.scrollV) {
+      this.parentElement.scrollTop -= event.movementY;
+    }
+    if (this.scrollH) {
+      this.parentElement.scrollLeft -= event.movementX;
+    }
+  };
+
+  private onDocumentMouseUpFn = () => {
+    // console.log('onDocumentMouseUpFn()');
+    this.parentElement.removeEventListener('mousemove', this.onMouseMoveFn);
+
+    document.removeEventListener('mouseup', this.onDocumentMouseUpFn);
+    document.removeEventListener('dragend', this.onDocumentDragEndFn);
+
+    this.resetCursor();
+
+    this.parentElement.classList.remove('touch-scroll_on');
+    this.parentElement.classList.remove('touch-scroll_move');
+
+    this.touchScrollOn = false;
+    this.touchScrollMove = false;
+  };
+
+  /**
+   * When element inside host is being dragged, document 'mouseup' event is not fired,
+   * so we call its listener manually in the 'dragend' callback
+   */
+  private onDocumentDragEndFn = () => {
+    // console.log('onDocumentDragEndFn()');
+    this.onDocumentMouseUpFn();
+  };
+
+  /***** Cursor tip *****/
+  private setCursor(): void {
+    this.cursorTimer = setTimeout(() => {
+      if (this.parentElement) {
+        this.parentElement.style.cursor = 'grab';
+      }
+    }, this.tipDelayInMs);
+  }
+
+  private resetCursor(): void {
+    if (this.cursorTimer) {
+      clearTimeout(this.cursorTimer);
+    }
+    this.cursorTimer = null;
+
+    if (this.parentElement) {
+      this.parentElement.style.cursor = null;
+    }
   }
 }
