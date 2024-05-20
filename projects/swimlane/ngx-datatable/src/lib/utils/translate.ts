2eed0a4a1cd32da812233cd68fce8b8c75d11a3f
Nick Fedorov <nikola_fed1@mail.ru>
2024-05-20 11:38:07 +0300

    refs #26914 get rid of vendor prefixes when translating rows (20.1.3)

--- projects/swimlane/ngx-datatable/src/lib/utils/translate.ts
+++ projects/swimlane/ngx-datatable/src/lib/utils/translate.ts
@@ -2,10 +2,10 @@ import { getVendorPrefixedName } from './prefixes';
 import { camelCase } from './camel-case';
 
 // browser detection and prefixing tools
-const transform = typeof window !== 'undefined' ? getVendorPrefixedName('transform') : undefined;
-const backfaceVisibility = typeof window !== 'undefined' ? getVendorPrefixedName('backfaceVisibility') : undefined;
-const hasCSSTransforms = typeof window !== 'undefined' ? !!getVendorPrefixedName('transform') : undefined;
-const hasCSS3DTransforms = typeof window !== 'undefined' ? !!getVendorPrefixedName('perspective') : undefined;
+const transform = 'transform'; // typeof window !== 'undefined' ? getVendorPrefixedName('transform') : undefined;
+const backfaceVisibility = 'backfaceVisibility'; // typeof window !== 'undefined' ? getVendorPrefixedName('backfaceVisibility') : undefined;
+const hasCSSTransforms = 'transform'; // typeof window !== 'undefined' ? !!getVendorPrefixedName('transform') : undefined;
+const hasCSS3DTransforms = 'perspective'; // typeof window !== 'undefined' ? !!getVendorPrefixedName('perspective') : undefined;
 const ua = typeof window !== 'undefined' ? window.navigator.userAgent : 'Chrome';
 const isSafari = /Safari\//.test(ua) && !/Chrome\//.test(ua);
 
